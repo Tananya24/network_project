d0a1a7e9e87e913728334f52d1ccff0dd3c62bfe
Tananya24 <tananya.pi@ku.th>
2022-01-08 14:21:10 +0700

    first

---
+++ game.py
@@ -0,0 +1,38 @@
+def init(self,id):
+    self.p1Went  = False
+    self.p2Went  = False
+    self.ready  = False
+    self.id = id
+    self.move = [None, None]
+    self.win = [0,0]
+    self.ties = 0
+def get_play_move(self,p):
+    return self.move[p]
+def player(self,player,move):
+    self.move[player] = move
+    if player == 0: self.p1Went = True
+    else: self.p2Went = True
+def connected(self):
+    return self.ready
+def bothWent(self):
+    return self.p1Went and self.p2Went
+def winner(self):
+    p1 = self.move[0].upper()[0]
+    p2 = self.move[1].upper()[0]
+    winner = -1
+    if p1 == "R" and p2 == "S":
+        winner = 0
+    elif p1 == "S" and p2 == "R":
+        winner = 1
+    elif p1 == "P" and p2 == "R":
+        winner = 0
+    elif p1 == "R" and p2 == "P":
+        winner  = 1
+    elif p1 == "S" and p2 == "P":
+        winner = 0
+    elif p1 ==  "P" and p2 == "S":
+        winner = 1
+    return winner
+def resetWent(self):
+    self.p1Went =False
+    self.p2Went = False
